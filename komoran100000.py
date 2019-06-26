98f6a605a8a6158a016315e066fd1004d5de4d38
dhy00085 <dhy00085@naver.com>
2019-06-27 02:32:46 +0900

    형태소 분석기 test

---
+++ komoran100000.py
@@ -0,0 +1,14 @@
+import time
+start = time.time()
+
+from konlpy.tag import Komoran
+H = Komoran()
+sentence = u'내년도 최저임금을 기존 방식대로 전체 업종에 동일하게 적용하기로 결정했다.\
+최저임금의 업종별 차등 적용을 요구해온 사용자위원들은 이에 반발해 전원회의에서 퇴장했다.\
+최저임금위원회 사용자위원들은 이날 오후 정부세종청사에서 열린 최저임금위원회 제5차 전원회의 도중 퇴장해 기자들과 만나 \
+"금일 최저임금위원회는 최저임금 고시에 월 환산액을 병기하고 2020년 최저임금을 모든 업종에 동일하게 적용하기로 결정했다"고 밝혔다.'
+sentences = [sentence] * 100000
+strat_time = time.time()
+morphs = [H.pos(sentence) for sentence in sentences]
+elapsed_time = time.time() - strat_time
+print('morphs_processor name = %20s, %.5f secs' % ('komoran', elapsed_time))
